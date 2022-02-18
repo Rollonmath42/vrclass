18686673466f7933b1416d3890fd0a2e91fa5a91
legitcondition <46453482+legitcondition@users.noreply.github.com>
2022-02-18 13:15:20 -0600

    Delete VRClass/Intermediate directory
    Shouldn't be here

--- VRClass/Intermediate/Build/Win64/VRClassEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.31.31104
-#include "C:/Users/Kelley-PC/Documents/Unreal Projects/vrclass/VRClass/Intermediate/Build/Win64/VRClassEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
