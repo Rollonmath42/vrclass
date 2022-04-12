0d57c5582cfd741c9607845f3e9919549d3b02f1
Rollonmath42 <supercarlos2264@yahoo.com>
2022-04-12 15:45:15 -0500

    jeoparody complete

--- Server/VRServer.js
+++ Server/VRServer.js
@@ -8,6 +8,7 @@ const database = require('./sql_dao.js');
 const url = require("url");
 const crypto = require('crypto');
 
+let activity_switched = false;
 let activity_database = {
     "carlostwo" : [{
         name : "Test Jeoparody",
@@ -201,6 +202,148 @@ sqlServer.get("/vrclass_login", (req, res) => {
 
 sqlServer.get("/vrclass_activity_list", (req, res) => {
     res.send(create_response("OK", 0, activity_database["carlostwo"]));
+});
+
+sqlServer.get("/vrclass_switch_activity_list", (req, res) => {
+    if(!activity_switched) {
+        activity_database["carlostwo"] = [{
+        name : "Test Jeoparody",
+        gametype : "Jeoparody",
+        data : {
+            "0" : {
+                name : "Song Lyrics",
+                tiers : {
+                    "0" : "Hey! I just met you,<br>and this is crazy,<br>but here's my number.",
+                    "1" : "I'm still a rock star,<br>I got my rock moves,<br>and I don't need you.",
+                    "2" : "Back to the street where we began,<br>feeling as good as lovers can, you know",
+                    "3" : "I don't ever wanna feel<br>like I did that day.<br>Take me to the place I love."
+                }
+            },
+            "1" : {
+                name : "Video Game Quotes",
+                tiers : {
+                    "0" : "Snake? SNAKE? SNAAAAAAAKE?",
+                    "1" : "Do you get to the cloud district often?",
+                    "2" : "Devotion inspires bravery.<br>Bravery inspires sacrifice.<br>Sacrifice leads to death.",
+                    "3" : "Just bought a pony made of diamonds,<br>because I'm rich. So, you know.<br>That's cool. Kay, bye."
+                }
+            },
+            "2" : {
+                name : "Math Problems",
+                tiers : {
+                    "0" : "The sine of 0 degrees is equal to<br>this value",
+                    "1" : "The square of 25 is this value",
+                    "2" : "The sine of 6120 degrees is equal<br>to this value",
+                    "3" : "The integral of secant of<br>an angle theta with respect to theta<br>is this equation"
+                }
+            },
+            "3" : {
+                name : "Geography",
+                tiers : {
+                    "0" : "This is the capital of France.",
+                    "1" : "This is the capital of Puerto Rico",
+                    "2" : "Helsinki is the capital of this country",
+                    "3" : "San Jose is the capital of this country"
+                }
+            }
+        }
+        }];
+    }
+    else {
+        activity_database["carlostwo"] = [{
+            name : "Test Jeoparody",
+            gametype : "Jeoparody",
+            data : {
+                "0" : {
+                    name : "category 0",
+                    tiers : {
+                        "0" : "this is cat 0 tier 0",
+                        "1" : "this is cat 0 tier 1",
+                        "2" : "this is cat 0 tier 2",
+                        "3" : "this is cat 0 tier 3"
+                    }
+                },
+                "1" : {
+                    name : "category 1",
+                    tiers : {
+                        "0" : "this is cat 1 tier 0",
+                        "1" : "this is cat 1 tier 1",
+                        "2" : "this is cat 1 tier 2",
+                        "3" : "this is cat 1 tier 3"
+                    }
+                },
+                "2" : {
+                    name : "category 2",
+                    tiers : {
+                        "0" : "this is cat 2 tier 0",
+                        "1" : "this is cat 2 tier 1",
+                        "2" : "this is cat 2 tier 2",
+                        "3" : "this is cat 2 tier 3"
+                    }
+                },
+                "3" : {
+                    name : "category 3",
+                    tiers : {
+                        "0" : "this is cat 3 tier 0",
+                        "1" : "this is cat 3 tier 1",
+                        "2" : "this is cat 3 tier 2",
+                        "3" : "this is cat 3 tier 3"
+                    }
+                }
+            }
+        },
+        {
+            name : "Test Activity",
+            gametype : "BasicQuiz",
+            data : {
+                questions : [{
+                    question : "This is a test question",
+                    answers : [
+                        {
+                            text : "This is the correct answer",
+                            isCorrect : true
+                        },
+                        {
+                            text : "This is the wrong answer",
+                            isCorrect : false
+                        },
+                        {
+                            text : "This is another wrong answer",
+                            isCorrect : false
+                        },
+                        {
+                            text : "Nope",
+                            isCorrect : false
+                        }
+                    ]
+                }, 
+                {
+                    question : "This is the second question",
+                    answers : [
+                        {
+                            text : "Don't pick me",
+                            isCorrect : false
+                        },
+                        {
+                            text : "I'm not correct",
+                            isCorrect : false
+                        },
+                        {
+                            text : "Even if I was the right answer, I wouldn't tell you!",
+                            isCorrect : true
+                        },
+                        {
+                            text : "Where am I?",
+                            isCorrect : false
+                        }
+                    ]
+                }]
+            }
+        }];
+    }
+
+    activity_switched = !activity_switched;
+    res.send(create_response("OK", 0, {switched : activity_switched}));
 });
 
 function create_response(message, error, data) {
